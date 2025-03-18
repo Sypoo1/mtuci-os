eeb34792e1aa74944b8dabbb6d7f1457f7b4e6e5
Rustam <sypooit@gmail.com>
2025-03-18 04:53:14 +0300

    done 2.4

---
+++ labs/2/4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/wait.h>
+
+// Функция для вывода информации о процессе
+void print_info(const char *name) {
+    char timeStr[9]; // Формат "HH:MM:SS"
+    time_t now = time(NULL);
+    strftime(timeStr, sizeof(timeStr), "%H:%M:%S", localtime(&now));
+    printf("%s: PID = %d, PPID = %d, Время = %s\n",
+        name, getpid(), getppid(), timeStr);
+}
+
+int main(void) {
+    pid_t pid1, pid2;
+
+    // Создаём первый дочерний процесс
+    pid1 = fork();
+
+    if (pid1 == 0) {
+        print_info("Первый дочерний процесс");
+        system("arp -a");
+        exit(EXIT_SUCCESS);
+    }
+
+    // Создаём второй дочерний процесс
+    pid2 = fork();
+
+    if (pid2 == 0) {
+        print_info("Второй дочерний процесс");
+
+        execl("./3", "3", (char *)NULL);
+        // Если execl вернул управление, значит произошла ошибка
+        perror("Ошибка exec");
+        exit(EXIT_FAILURE);
+    }
+
+    print_info("Родительский процесс");
+
+    system("ps -x");
+
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
+
+    return 0;
+}
